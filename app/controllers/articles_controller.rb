9b1f66fc65ff81a5174797a807a997fabd099c94
Leonardo Cisternas <leocisternasa@gmail.com>
2022-08-20 11:03:41 -0400

    Edit and destroy articles / Se agrega editar y eliminar artículos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,27 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  def new
-    @article =   Article.new
-    @article.title = "Demo"
+  def edit
+    @article = Article.find(params[:id])
+  end 
+  def new 
+    @article = Article.new 
   end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], content: params[:article][:content])
+    redirect_to @article
+
+end
     def create
        @article = Article.create(title: params[:article][:title], content: params[:article][:content])
        render json: @article
 
   end 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   get "/bienvenida", to: "home#index"
 
+  root to: "home#index"
+
   get "articles/new", to: "articles#new"
   get "articles/:id", to: "articles#show" 
+  get "articles/:id/edit", to: "articles#edit"
+
+  patch "articles/:id" , to: "articles#update", as: :article
   post "articles", to: "articles#create" 
+  delete "articles/:id", to: "articles#destroy"
 end
