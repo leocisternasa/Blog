8cb452bfe9b47f6abe8222dcd20832f80ea50511
Leonardo Cisternas <leocisternasa@gmail.com>
2022-08-20 11:19:54 -0400

    Using before_action in order to clean the code and don't repeat lines / usando before_action para limpiar el codigo y no repetir

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,16 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, except: [:new,:create]
   def show
-    @article = Article.find(params[:id])
+
   end
   def edit
-    @article = Article.find(params[:id])
+    
   end 
   def new 
     @article = Article.new 
   end
   def update
-    @article = Article.find(params[:id])
+
     @article.update(title: params[:article][:title], content: params[:article][:content])
     redirect_to @article
 
@@ -20,9 +21,11 @@ end
 
   end 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to root_path
   
+  end
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
