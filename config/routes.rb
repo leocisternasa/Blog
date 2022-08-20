1bf98459d2e07a6d9f9c52cd6152d6a5b257128a
Leonardo Cisternas <leocisternasa@gmail.com>
2022-08-20 02:02:40 -0400

    Show articles by id in routes / mostrar articulos mediante el ID en las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "/bienvenida", to: "home#index"
 
   get "articles/new", to: "articles#new"
+  get "articles/:id", to: "articles#show" 
   post "articles", to: "articles#create" 
 end
