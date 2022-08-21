894ab75c272e1429bc555c25c625e8c34968ce38
Leonardo Cisternas <leocisternasa@gmail.com>
2022-08-20 20:57:38 -0400

    Adding relation between articles and user and showing who was the creator of each article / añadiendo relacion entre articulos y usuarios y mostrando quien fue el creador de cada artículo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, except: [:new,:create,:index]
+  before_action :find_article, except: [:new,:create,:index,:from_author]
   before_action :authenticate_user!, only: [:new,:create,:edit,:update,:destroy]
   def index
     @articles = Article.all
@@ -20,14 +20,19 @@ class ArticlesController < ApplicationController
 
 end
     def create
-       @article = Article.create(title: params[:article][:title], content: params[:article][:content])
-       render json: @article
+       @article = current_user.articles.create(title: params[:article][:title], content: params[:article][:content])
+       redirect_to @article
+
+       #También se puede asociar el artículo con el creador mediante @article = Article.create(title: params[:article][:title], content: params[:article][:content], user: current_user) pero la mejor forma de hacerlo es la que utilizamos arriba.
 
   end 
   def destroy
     @article.destroy
     redirect_to root_path
   
+  end
+  def from_author
+       @user = User.find(params[:user_id])
   end
   def find_article
     @article = Article.find(params[:id])
